ea085363bf6cbd99e0aa45699420f3dbd524f619
Adrian Olalde <adrianolaldeortega@gmail.com>
2022-03-08 16:08:42 -0600

    Calculadora terminada

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -10,12 +10,28 @@ import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    var firstValue: Double = 0.0
+    var operador: Char = '0'
+    var secondValue: Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.buttonIgual.setOnClickListener{
+            when(operador){
+                '+' -> {binding.editTextNumber.setText((firstValue + binding.editTextNumber.text.toString().toDouble()).toString())}
+                '-' -> {binding.editTextNumber.setText((firstValue - binding.editTextNumber.text.toString().toDouble()).toString())}
+                '÷' -> {binding.editTextNumber.setText((firstValue / binding.editTextNumber.text.toString().toDouble()).toString())}
+                'x' -> {binding.editTextNumber.setText((firstValue * binding.editTextNumber.text.toString().toDouble()).toString())}
+                else ->{binding.editTextNumber.setText("Error")}
+            }
+            operador = '0'
+            firstValue = 0.0
+        }
+        binding.buttonPorciento.setOnClickListener{
+            binding.editTextNumber.setText(percentage(operador))
+        }
 
     }
     fun NumberInput(view: View){
@@ -30,5 +46,27 @@ class MainActivity : AppCompatActivity() {
     }
     fun DeleteText(view: View){
         binding.editTextNumber.setText(binding.editTextNumber.text.dropLast(1))
+    }
+    fun operatorClicked(view: View){
+        val boton = view as Button
+        operador = when(boton.id){
+            binding.buttonDivision.id -> {'÷'}
+            binding.buttonMulti.id -> {'x'}
+            binding.buttonSuma.id -> {'+'}
+            binding.Resta.id -> {'-'}
+            else -> {'0'}
+        }
+        firstValue = binding.editTextNumber.text.toString().toDouble()
+        binding.editTextNumber.text.clear()
+    }
+    fun percentage(operadorAUsar: Char): String{
+        when (operador){
+            '0' -> {return "${binding.editTextNumber.text.toString().toDouble()/100}"}
+            '+' -> {return "${firstValue + (firstValue*binding.editTextNumber.text.toString().toDouble()/100)}"}
+            '-' -> {return "${firstValue - (firstValue*binding.editTextNumber.text.toString().toDouble()/100)}"}
+            '÷' -> {return "${firstValue / (firstValue*binding.editTextNumber.text.toString().toDouble()/100)}"}
+            'x' -> {return "${firstValue * (firstValue*binding.editTextNumber.text.toString().toDouble()/100)}"}
+            else -> {return "ERROR"}
+        }
     }
 }
